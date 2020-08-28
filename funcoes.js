9544361904a34d874f4974ecf82bf91d76dd85eb
FilipeTenorioB <50412204+FilipeTenorioB@users.noreply.github.com>
2020-08-28 15:57:47 -0300

    Adição faltando checkbox, atualização na estilização.
    Tentativa de incluir 'checkbox' na criação do 'li'. Atualização no arquivo 'css'.

--- funcoes.js
+++ funcoes.js
@@ -1,18 +1,47 @@
+/* Adicionar tarefas */
+
 var botaoAdicionar = document.querySelector("#bto-adicionartarefa");
 
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-    adicionaTarefaNaLista(tarefa);
+
+    var tarefa = document.querySelector("#local-adicionartarefa");
+    var tarefaDigitada = tarefa.value;
+    adicionaTarefaNaLista(tarefaDigitada);
 });
 
-function adicionaTarefaNaLista(tarefa){
-    var tarefaLi = montaLi(tarefa);
-    var lista = document.querySelector("#lista-tarefas");
-    lista.appendChild(tarefaLi);
+function montaLi(tarefa, classe){
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+
+    var li = document.createElement("li");
+    li.textContent = tarefa;
+    li.classList.add(classe);
+    return li;
 }
 
-function montaLi(tarefa){
-    var td = document.createElement("li");
-    td.textContent = tarefa;
-    return td;
+function adicionaTarefaNaLista(tarefa){
+    var lista = document.querySelector("#lista-tarefas");
+    lista.appendChild(montaLi(tarefa, "tarefas-lista"));
 }
+
+function criarCheckbox() {
+    var check = document.createElement("INPUT");
+    check.setAttribute("type", "checkbox");
+    document.body.appendChild(check);
+  }
+
+  /* onclick="criarCheckbox()" no button no HTML  */
+
+/* Remover tarefas */
+
+var tarefa = document.querySelector("li");
+tarefa.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+});
+
+/* Editar tarefas */
+
