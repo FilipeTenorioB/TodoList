733c6346c68b46464ce5166b3b99c0b2da1a1fc0
FilipeTenorioB <50412204+FilipeTenorioB@users.noreply.github.com>
2020-08-28 10:08:38 -0300

    Evento - Click
    Adicionei o evento de 'click' ao botão '+', relacionando com a função de de adicionar um novo elemento em minha lista.

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,10 @@
+var botaoAdicionar = document.querySelector("#bto-adicionartarefa");
+
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    adicionaTarefaNaLista(tarefa);
+});
+
 function adicionaTarefaNaLista(tarefa){
     var tarefaLi = montaLi(tarefa);
     var lista = document.querySelector("#lista-tarefas");
